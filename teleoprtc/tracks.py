e9889c8e9cde5e0ceb06d963b1efd907afec6042
Cameron Clough <cameronjclough@gmail.com>
2024-06-04 20:04:05 +0100

    use typing.Tuple

--- teleoprtc/tracks.py
+++ teleoprtc/tracks.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 import time
 import fractions
-from typing import Any, Optional
+from typing import Any, Optional, Tuple
 
 import aiortc
 from aiortc.mediastreams import VIDEO_CLOCK_RATE, VIDEO_TIME_BASE
@@ -12,7 +12,7 @@ def video_track_id(camera_type: str, track_id: str) -> str:
   return f"{camera_type}:{track_id}"
 
 
-def parse_video_track_id(track_id: str) -> tuple[str, str]:
+def parse_video_track_id(track_id: str) -> Tuple[str, str]:
   parts = track_id.split(":")
   if len(parts) != 2:
     raise ValueError(f"Invalid video track id: {track_id}")
