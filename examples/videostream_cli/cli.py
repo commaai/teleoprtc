5fad9f56f71cc61ae8ed62f2802c4b2e5091f388
stefpi <19478336+stefpi@users.noreply.github.com>
2026-07-07 18:57:45 -0700

    fix example, remove other example

--- examples/videostream_cli/cli.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-
-def main() -> None:
-  raise SystemExit("videostream_cli has not been ported to the libdatachannel backend yet")
-
-
-if __name__ == "__main__":
-  main()
